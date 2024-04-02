77c9f1394947b1fb98a94465f3b3e1199bfc63b2
Markus Zimmermann <markus.zimmermann@symflower.com>
2024-04-02 11:51:30 +0200

    Rewrite binary with go-flags for easier parsing of arguments and options
    I spent more time figuring out how to do validation and how to set options without global variables than actually being productive. Might be me, but when all tutorials tell you to use global variables, i say no.

---
+++ cmd/eval-symflower-codegen-testing/cmd/command.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"log"
+	"os"
+
+	"github.com/jessevdk/go-flags"
+)
+
+// Command holds the root command.
+type Command struct {
+	Evaluate `command:"evaluate" description:"Run an evaluation, by default with all defined models, repositories and tasks."`
+}
+
+// Execute executes the root command.
+func Execute() {
+	var parser = flags.NewNamedParser("eval-symflower-codegen-testing", flags.Default)
+	parser.LongDescription = "Command to manage, update and actually execute the `eval-symflower-codegen-testing` evaluation benchmark."
+	if _, err := parser.AddGroup("Common command options", "", &Command{}); err != nil {
+		log.Fatalf("Could not add arguments group: %+v", err)
+	}
+
+	// Print the help, when there is no active command.
+	parser.SubcommandsOptional = true
+
+	if _, err := parser.Parse(); err != nil {
+		if e, ok := err.(*flags.Error); ok && e.Type == flags.ErrHelp {
+			return
+		}
+
+		log.Fatalf("Could not parse arguments: %+v", err)
+	}
+	if parser.Active == nil {
+		parser.WriteHelp(os.Stdout)
+	}
+}
--- cmd/eval-symflower-codegen-testing/cmd/evaluate.go
+++ cmd/eval-symflower-codegen-testing/cmd/evaluate.go
@@ -5,7 +5,6 @@ import (
 	"path/filepath"
 	"sort"
 
-	"github.com/spf13/cobra"
 	"golang.org/x/exp/maps"
 
 	"github.com/symflower/eval-symflower-codegen-testing/evaluate"
@@ -13,33 +12,30 @@ import (
 	"github.com/symflower/eval-symflower-codegen-testing/model"
 )
 
-var commandEvalute = &cobra.Command{
-	Use:   "evaluate",
-	Short: "Run an evaluation, by default with all defined models, repositories and tasks.",
-	Run: func(command *cobra.Command, arguments []string) {
-		// Gather languages.
-		languageIDs := maps.Keys(language.Languages)
-		sort.Strings(languageIDs)
+// Evaluate holds the "evaluation" command.
+type Evaluate struct{}
 
-		// Gather models.
-		modelIDs := maps.Keys(model.Models)
-		sort.Strings(modelIDs)
+func (command *Evaluate) Execute(args []string) (err error) {
+	// Gather languages.
+	languageIDs := maps.Keys(language.Languages)
+	sort.Strings(languageIDs)
 
-		// Check that models and languages can be evaluated by executing the "plain" repositories.
-		log.Printf("Checking that models and languages can used for evaluation")
-		for _, languageID := range languageIDs {
-			for _, modelID := range modelIDs {
-				model := model.Models[modelID]
-				language := language.Languages[languageID]
+	// Gather models.
+	modelIDs := maps.Keys(model.Models)
+	sort.Strings(modelIDs)
 
-				if err := evaluate.EvaluateRepository(model, language, filepath.Join("testdata", language.ID(), "plain")); err != nil {
-					log.Fatalf("%+v", err)
-				}
+	// Check that models and languages can be evaluated by executing the "plain" repositories.
+	log.Printf("Checking that models and languages can used for evaluation")
+	for _, languageID := range languageIDs {
+		for _, modelID := range modelIDs {
+			model := model.Models[modelID]
+			language := language.Languages[languageID]
+
+			if err := evaluate.EvaluateRepository(model, language, filepath.Join("testdata", language.ID(), "plain")); err != nil {
+				log.Fatalf("%+v", err)
 			}
 		}
-	},
-}
+	}
 
-func init() {
-	commandRoot.AddCommand(commandEvalute)
+	return nil
 }
