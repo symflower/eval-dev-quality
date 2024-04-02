bfbccee037550db0aea7d3469b7fcbf52be5f13e
Markus Zimmermann <markus.zimmermann@symflower.com>
2024-04-02 12:24:19 +0200

    Allow to choose languages and models CLI argumentss

--- cmd/eval-symflower-codegen-testing/cmd/evaluate.go
+++ cmd/eval-symflower-codegen-testing/cmd/evaluate.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"path/filepath"
 	"sort"
+	"strings"
 
 	"golang.org/x/exp/maps"
 
@@ -13,21 +14,48 @@ import (
 )
 
 // Evaluate holds the "evaluation" command.
-type Evaluate struct{}
+type Evaluate struct {
+	// Languages determines which language should be used for the evaluation, or empty if all languages should be used.
+	Languages []string `long:"language" description:"Evaluate with this language. By default all languages are used."`
+	// Models determines which models should be used for the evaluation, or empty if all models should be used.
+	Models []string `long:"model" description:"Evaluate with this model. By default all models are used."`
+}
 
 func (command *Evaluate) Execute(args []string) (err error) {
 	// Gather languages.
-	languageIDs := maps.Keys(language.Languages)
-	sort.Strings(languageIDs)
+	if len(command.Languages) == 0 {
+		command.Languages = maps.Keys(language.Languages)
+	} else {
+		for _, languageID := range command.Languages {
+			if _, ok := language.Languages[languageID]; !ok {
+				ls := maps.Keys(language.Languages)
+				sort.Strings(ls)
+
+				log.Fatalf("ERROR: language %s does not exist. Valid languages are: %s", languageID, strings.Join(ls, ", "))
+			}
+		}
+	}
+	sort.Strings(command.Languages)
 
 	// Gather models.
-	modelIDs := maps.Keys(model.Models)
-	sort.Strings(modelIDs)
+	if len(command.Models) == 0 {
+		command.Models = maps.Keys(model.Models)
+	} else {
+		for _, modelID := range command.Models {
+			if _, ok := model.Models[modelID]; !ok {
+				ms := maps.Keys(model.Models)
+				sort.Strings(ms)
+
+				log.Fatalf("ERROR: model %s does not exist. Valid models are: %s", modelID, strings.Join(ms, ", "))
+			}
+		}
+	}
+	sort.Strings(command.Models)
 
 	// Check that models and languages can be evaluated by executing the "plain" repositories.
 	log.Printf("Checking that models and languages can used for evaluation")
-	for _, languageID := range languageIDs {
-		for _, modelID := range modelIDs {
+	for _, languageID := range command.Languages {
+		for _, modelID := range command.Models {
 			model := model.Models[modelID]
 			language := language.Languages[languageID]
 
