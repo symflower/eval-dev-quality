a8e7e40bfb97b50322288d79bb89d34196a998df
Simon Bauer <simon.bauer@symflower.com>
2024-04-17 11:23:13 +0200

    refactor, Move existing metrics to assessments
    Closes #34

--- evaluate/metrics/assessment_test.go
+++ evaluate/metrics/assessment_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/zimmski/osutil/bytesutil"
 )
 
 func TestAssessmentsAdd(t *testing.T) {
@@ -112,5 +113,100 @@ func TestMerge(t *testing.T) {
 		ExpectedC: map[AssessmentKey]uint{
 			AssessmentKeyNoExcessResponse: 2,
 		},
+	})
+}
+
+func TestAssessmentString(t *testing.T) {
+	type testCase struct {
+		Name string
+
+		Assessment Assessments
+
+		ExpectedString string
+	}
+
+	validate := func(t *testing.T, tc *testCase) {
+		t.Run(tc.Name, func(t *testing.T) {
+			actualString := tc.Assessment.String()
+
+			assert.Equal(t, tc.ExpectedString, actualString)
+		})
+	}
+
+	validate(t, &testCase{
+		Name: "Initial Metrics",
+
+		Assessment: NewAssessments(),
+
+		ExpectedString: "files-executed=0, files-problems=0, coverage-statement=0, no-excess-response=0",
+	})
+
+	validate(t, &testCase{
+		Name: "Empty Metrics",
+
+		Assessment: Assessments{
+			AssessmentKeyCoverageStatement: 1,
+			AssessmentKeyFilesExecuted:     2,
+			AssessmentKeyFilesProblems:     3,
+			AssessmentKeyNoExcessResponse:  4,
+		},
+
+		ExpectedString: "files-executed=2, files-problems=3, coverage-statement=1, no-excess-response=4",
+	})
+}
+
+func TestFormatStringCSV(t *testing.T) {
+	type testCase struct {
+		Name string
+
+		AssessmentPerModel map[string]Assessments
+
+		ExpectedString string
+	}
+
+	validate := func(t *testing.T, tc *testCase) {
+		t.Run(tc.Name, func(t *testing.T) {
+			actualString, err := FormatStringCSV(tc.AssessmentPerModel)
+			assert.NoError(t, err)
+
+			assert.Equal(t, bytesutil.StringTrimIndentations(tc.ExpectedString), actualString)
+		})
+	}
+
+	validate(t, &testCase{
+		Name: "Single Empty Model",
+
+		AssessmentPerModel: map[string]Assessments{
+			"Model": Assessments{},
+		},
+
+		ExpectedString: `
+			model,files-executed,files-problems,coverage-statement,no-excess-response
+			Model,0,0,0,0
+		`,
+	})
+	validate(t, &testCase{
+		Name: "Multiple Models",
+
+		AssessmentPerModel: map[string]Assessments{
+			"ModelA": Assessments{
+				AssessmentKeyCoverageStatement: 1,
+				AssessmentKeyFilesExecuted:     2,
+				AssessmentKeyFilesProblems:     3,
+				AssessmentKeyNoExcessResponse:  4,
+			},
+			"ModelB": Assessments{
+				AssessmentKeyCoverageStatement: 1,
+				AssessmentKeyFilesExecuted:     2,
+				AssessmentKeyFilesProblems:     3,
+				AssessmentKeyNoExcessResponse:  4,
+			},
+		},
+
+		ExpectedString: `
+			model,files-executed,files-problems,coverage-statement,no-excess-response
+			ModelA,2,3,1,4
+			ModelB,2,3,1,4
+		`,
 	})
 }
