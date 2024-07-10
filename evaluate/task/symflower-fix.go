3d04b4772ee9e64b91711ff5afb1b0c26ceb0fbf
Rui Azevedo <rui.azevedo@symflower.com>
2024-07-10 09:58:26 +0100

    refactor, Move the logic that runs the "symflower fix" into a helper, so it can be reused in other tasks
    Part of #201

--- evaluate/task/symflower-fix.go
+++ evaluate/task/symflower-fix.go
@@ -5,8 +5,10 @@ import (
 	"time"
 
 	pkgerrors "github.com/pkg/errors"
+	"github.com/symflower/eval-dev-quality/evaluate/metrics"
 	"github.com/symflower/eval-dev-quality/language"
 	"github.com/symflower/eval-dev-quality/log"
+	evaltask "github.com/symflower/eval-dev-quality/task"
 	"github.com/symflower/eval-dev-quality/tools"
 	"github.com/symflower/eval-dev-quality/util"
 )
@@ -28,4 +30,30 @@ func symflowerFix(logger *log.Logger, repositoryPath string, language language.L
 	}
 
 	return uint64(time.Since(start).Milliseconds()), nil
+}
+
+// ExecuteWithSymflowerFix runs the "symflower fix" command and calculates the new assessments.
+func ExecuteWithSymflowerFix(ctx evaltask.Context, logger *log.Logger, packagePath string) (assessments metrics.Assessments, problems []error, err error) {
+	// Run "symflower fix"  if the model response fails to execute.
+	logger.Print("model response alone failed execution, attempting to fix with \"symflower fix \"")
+
+	duration, err := symflowerFix(logger, packagePath, ctx.Language)
+	if err != nil {
+		return nil, problems, pkgerrors.WithStack(err)
+	}
+
+	coverage, ps, err := ctx.Language.Execute(logger, packagePath)
+	problems = append(problems, ps...)
+	if err != nil {
+		return nil, problems, pkgerrors.WithMessage(err, "symflower fix")
+	}
+	logger.Printf("with symflower repair: Executes tests with %d coverage objects", coverage)
+
+	// Symflower was able to fix a failure so now update the assessment with the improved results.
+	assessments = metrics.NewAssessments()
+	assessments[metrics.AssessmentKeyProcessingTime] = duration
+	assessments.Award(metrics.AssessmentKeyFilesExecuted)
+	assessments.AwardPoints(metrics.AssessmentKeyCoverage, coverage)
+
+	return assessments, problems, nil
 }
