62d7d9dd33eba737b69e53c649ffe1c498367619
Simon Bauer <simon.bauer@symflower.com>
2024-07-26 12:29:46 +0200

    Ignore Openrouter base models if an instruction-tuned model is available

--- scripts/openrouter-models/main.go
+++ scripts/openrouter-models/main.go
@@ -5,12 +5,31 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"slices"
+	"sort"
 	"strings"
 
 	"github.com/symflower/eval-dev-quality/model"
 	"github.com/symflower/eval-dev-quality/provider/openrouter"
+	"golang.org/x/exp/maps"
 )
+
+func baseModel(name string) string {
+	return strings.TrimSuffix(strings.TrimSuffix(name, "-instruct"), "-chat")
+}
+
+func isBaseModel(name string) bool {
+	return baseModel(name) == name
+}
+
+func containsInstructModel(models map[string]bool, name string) string {
+	if models[name+"-instruct"] {
+		return name + "-instruct"
+	} else if models[name+"-chat"] {
+		return name + "-chat"
+	}
+
+	return ""
+}
 
 var ignoredModels = []string{
 	// Alias models.
@@ -46,7 +65,7 @@ func main() {
 		panic(err)
 	}
 
-	var models []model.Model
+	models := map[string]bool{}
 	for _, model := range allModels {
 		if isIgnored(model) {
 			fmt.Printf("ignoring %q\n", model.ID())
@@ -54,11 +73,22 @@ func main() {
 			continue
 		}
 
-		models = append(models, model)
+		if !isBaseModel(model.ID()) && models[baseModel(model.ID())] {
+			baseModelName := baseModel(model.ID())
+			fmt.Printf("ignoring %q for instruct model %q\n", baseModelName, model.ID())
+
+			delete(models, baseModelName)
+		} else if instructModelName := containsInstructModel(models, model.ID()); instructModelName != "" {
+			fmt.Printf("ignoring %q for instruct model %q\n", model.ID(), instructModelName)
+
+			continue
+		}
+
+		models[model.ID()] = true
 	}
-	slices.SortFunc(models, func(a, b model.Model) int {
-		return strings.Compare(a.ID(), b.ID())
-	})
+
+	modelNames := maps.Keys(models)
+	sort.Strings(modelNames)
 
 	csvFile, err := os.Create("openrouter.csv")
 	if err != nil {
@@ -69,7 +99,7 @@ func main() {
 	defer csvWriter.Flush()
 
 	csvWriter.Write([]string{"model"})
-	for _, model := range models {
-		csvWriter.Write([]string{model.ID()})
+	for _, model := range modelNames {
+		csvWriter.Write([]string{model})
 	}
 }
