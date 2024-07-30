05c0ff97d2cfd9cf307064ff2b3b8b065800b443
Simon Bauer <simon.bauer@symflower.com>
2024-07-30 09:12:54 +0200

    Update to latest Symflower version for improved static code repair for incorrect test packages

--- tools/symflower.go
+++ tools/symflower.go
@@ -91,7 +91,7 @@ func (*symflower) CheckVersion(logger *log.Logger, binaryPath string) (err error
 }
 
 // SymflowerVersionRequired holds the version of Symflower required for this revision of the evaluation.
-const SymflowerVersionRequired = "38240"
+const SymflowerVersionRequired = "38796"
 
 // RequiredVersion returns the required version of the tool.
 func (*symflower) RequiredVersion() string {
