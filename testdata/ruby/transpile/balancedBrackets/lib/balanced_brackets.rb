a4dc9fd0971b19dbf54fffb531c6f614919f1e1f
Rui Azevedo <rui.azevedo@symflower.com>
2024-08-07 11:24:33 +0100

    Transpile repository for Ruby
    Part of #300

---
+++ testdata/ruby/transpile/balancedBrackets/lib/balanced_brackets.rb
@@ -0,0 +1,4 @@
+# @param [String] string
+# @return [Boolean]
+def has_balanced_brackets(string)
+end
---
+++ testdata/ruby/transpile/pascalsTriangle/lib/pascals_triangle.rb
@@ -0,0 +1,4 @@
+# @param [Integer] rows
+# @return [Array<Array<Integer>>]
+def pascals_triangle(rows)
+end
