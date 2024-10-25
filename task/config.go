24f30a0b53967cdde97faec0e16130c915c3413b
Simon Bauer <simon.bauer@symflower.com>
2024-10-25 11:38:10 +0200

    refactor, Move repository configuration to generic task definition to allow extending it
    Part of #365

---
+++ task/config.go
@@ -0,0 +1,65 @@
+package task
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+
+	pkgerrors "github.com/pkg/errors"
+	"github.com/symflower/eval-dev-quality/util"
+	"github.com/zimmski/osutil"
+)
+
+// RepositoryConfiguration holds the configuration of a repository.
+type RepositoryConfiguration struct {
+	// Tasks holds the tasks supported by the repository.
+	Tasks []Identifier
+}
+
+// RepositoryConfigurationFileName holds the file name for a repository configuration.
+const RepositoryConfigurationFileName = "repository.json"
+
+// LoadRepositoryConfiguration loads a repository configuration from the given path.
+func LoadRepositoryConfiguration(path string, defaultTasks []Identifier) (config *RepositoryConfiguration, err error) {
+	if osutil.FileExists(path) != nil { // If we don't get a valid file, assume it is a repository directory and target the default configuration file name.
+		path = filepath.Join(path, RepositoryConfigurationFileName)
+	}
+
+	data, err := os.ReadFile(path)
+	if errors.Is(err, os.ErrNotExist) && len(defaultTasks) > 0 {
+		// Set default configuration.
+		return &RepositoryConfiguration{
+			Tasks: defaultTasks,
+		}, nil
+	} else if err != nil {
+		return nil, pkgerrors.Wrap(err, path)
+	}
+
+	config = &RepositoryConfiguration{}
+	if err := json.Unmarshal(data, &config); err != nil {
+		return nil, pkgerrors.Wrap(err, path)
+	} else if err := config.validate(defaultTasks); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+// validate validates the configuration.
+func (rc *RepositoryConfiguration) validate(validTasks []Identifier) (err error) {
+	if len(rc.Tasks) == 0 {
+		return pkgerrors.Errorf("empty list of tasks in configuration")
+	}
+
+	if len(validTasks) > 0 {
+		lookupTask := util.Set(validTasks)
+		for _, taskIdentifier := range rc.Tasks {
+			if !lookupTask[taskIdentifier] {
+				return pkgerrors.Errorf("task identifier %q unknown", taskIdentifier)
+			}
+		}
+	}
+
+	return nil
+}
